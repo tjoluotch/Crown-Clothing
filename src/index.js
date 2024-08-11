7e2f607bbbed3b4e92036b5fa9b4a411e9bd745a
tjoluotch <trevorjoluotch@gmail.com>
2024-08-11 01:35:13 +0100

    added all functionality to website

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
+import { Elements } from "@stripe/react-stripe-js";
+import { stripePromise } from "./utils/stripe/stripe.utils";
+// import { PersistGate } from "redux-persist/integration/react";
 
 import App from "./App";
 import { store, persistor } from "./store/store";
@@ -16,7 +18,9 @@ render(
     <Provider store={store}>
       {/*<PersistGate loading={null} persistor={persistor}>*/}
       <BrowserRouter>
-        <App />
+        <Elements stripe={stripePromise}>
+          <App />
+        </Elements>
       </BrowserRouter>
       {/*</PersistGate>*/}
     </Provider>
